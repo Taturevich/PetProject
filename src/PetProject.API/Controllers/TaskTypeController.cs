9d25239732bf53a7d66e80099f967062d82f3fcd
D9bolic <46654130+D9bolic@users.noreply.github.com>
2020-01-25 12:15:28 +0300

    Work with types added data annotations

--- src/PetProject.API/Controllers/TaskTypeController.cs
+++ src/PetProject.API/Controllers/TaskTypeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PetProject.DataAccess;
+using PetProject.DTO;
+using System.Threading.Tasks;
 
 namespace PetProject.Controllers
 {
@@ -7,27 +10,77 @@ namespace PetProject.Controllers
     [Route("api/[controller]")]
     public class TaskTypeController : ControllerBase
     {
-        private readonly PetContext petContext;
+        private readonly PetContext _petContext;
 
         public TaskTypeController(PetContext petContext)
         {
-            this.petContext = petContext;
+            _petContext = petContext;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetTypes()
+        {
+            return Ok(await _petContext.TaskTypes.AsQueryable().ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public async Task<IActionResult> GetType(string name)
+        {
+            return Ok(await _petContext.TaskTypes.AsQueryable().FirstOrDefaultAsync(x => x.Name == name));
         }
 
         [HttpPost]
-        public IActionResult AddType()
+        [Route("")]
+        public async Task<IActionResult> AddType([FromBody] TaskTypeDTO taskType)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _petContext.TaskTypes.AddAsync(new Domain.TaskType
+            {
+                Name = taskType.Name,
+                Description = taskType.Description,
+                PetPoints = taskType.PetPoints,
+                DefaultDuration = taskType.DefaultDuration
+            });
+
             return Ok();
         }
 
         [HttpPut]
-        public IActionResult UpdateType()
+        [Route("{name}")]
+        public async Task<IActionResult> UpdateType(string name, [FromBody] TaskTypeDTO taskType)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var dbTaskType = await _petContext.TaskTypes.AsQueryable().FirstOrDefaultAsync(x => x.Name == name);
+
+            if (dbTaskType is null)
+            {
+                return NotFound();
+            }
+
+            dbTaskType.Name = taskType.Name;
+            dbTaskType.Description = taskType.Description;
+            dbTaskType.PetPoints = taskType.PetPoints;
+            dbTaskType.DefaultDuration = taskType.DefaultDuration;
+
+            await _petContext.SaveChangesAsync();
+
             return Ok();
         }
 
         [HttpDelete]
-        public IActionResult Delete()
+        [Route("{name}")]
+        public async Task<IActionResult> Delete(string name)
         {
             return Ok();
         }
---
+++ src/PetProject.API/DTO/TaskTypeDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetProject.DTO
+{
+    public class TaskTypeDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public int PetPoints { get; set; }
+        
+        public DateTime DefaultDuration { get; set; }
+    }
+}
