995764adfe6da593596db85e3afe2fe6958070ed
Ivan <pollux094@gmail.com>
2020-01-25 10:23:42 +0300

    added domain models, pet status characteristics

---
+++ src/PetProject.Domain/PetCharacteristicDict.cs
@@ -0,0 +1,11 @@
+﻿namespace PetProject.Domain
+{
+    public class PetCharacteristicDict
+    {
+        public int CharId { get; set; }
+
+        public string Category { get; set; }
+
+        public string Characteristic { get; set; }
+    }
+}
