6be25b5b621f8d559b637aed4018beb774ba223a
Kanstantsin <replicant825@gmail.com>
2020-01-25 14:43:23 +0300

    Updated PetController and PetFeatureController.

--- src/PetProject.API/Controllers/PetFeatureController.cs
+++ src/PetProject.API/Controllers/PetFeatureController.cs
@@ -43,8 +43,8 @@ namespace PetProject.Controllers
         [HttpGet("{petId}")]
         public async Task<IActionResult> GetByPetId(int petId)
         {
-            var pet = await _petContext.Pets.FindAsync(petId);
-            var petFeatures = pet.PetFeatureAssignments.Select(pfa => pfa.PetFeature);
+            var petFeatures = _petContext.PetFeatureAssignments
+                .Where(pfa => pfa.PetId == petId);
 
             return Ok(petFeatures);
         }
