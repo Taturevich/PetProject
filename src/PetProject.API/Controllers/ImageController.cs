15130d3e9deac590c85dc281569f6f184cb3f06d
Ivan <pollux094@gmail.com>
2020-01-25 14:04:13 +0300

    added seed approach

---
+++ src/PetProject.API/Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetProject.DataAccess;
+
+namespace PetProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly PetContext _petContext;
+
+        public ImageController(PetContext petContext)
+        {
+            _petContext = petContext;
+        }
+
+        [HttpGet("petId")]
+        public async Task<IActionResult> GetByPetId(int petId)
+        {
+            var images = await _petContext.Images
+                .Where(p => p.PetId == petId)
+                .ToListAsync();
+            return Ok(images);
+        }
+    }
+}
