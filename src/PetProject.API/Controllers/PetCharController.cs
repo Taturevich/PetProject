79bc35412c9d1003b3ab853ff0f0603352ba63a3
Kanstantsin <replicant825@gmail.com>
2020-01-25 11:46:46 +0300

    Added PetCharController

---
+++ src/PetProject.API/Controllers/PetCharController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetCharController : ControllerBase
+    {
+        // GET: api/PetChar
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/PetChar/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PetChar
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PetChar/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
