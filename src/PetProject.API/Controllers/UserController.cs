e0960844d83f9afa76e369be0014680d624f6b4a
Ivan <pollux094@gmail.com>
2020-01-25 11:12:16 +0300

    trying to add swagger

---
+++ src/PetProject.API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PetProject.DataAccess;
+using PetProject.Domain;
+
+namespace PetProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly PetContext _petContext;
+
+        public UserController(PetContext petContext)
+        {
+            _petContext = petContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var pets = _petContext.Pets.ToList();
+            return Ok(pets);
+        }
+    }
+}
