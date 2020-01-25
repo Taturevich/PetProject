276a41da47c3c7d5739f8dc5ddc305452f9992c1
Ilya Kaliaskin <ilya_kaliaskin@epam.com>
2020-01-25 11:13:20 +0300

    Update context with PetFeatures

--- PetProject.DataAccess/PetContext.cs
+++ PetProject.DataAccess/PetContext.cs
@@ -9,6 +9,14 @@ namespace PetProject.DataAccess
 
         public DbSet<TaskType> TaskTypes { get; set; }
 
+        public DbSet<Pet> Pets { get; set; }
+
+        public DbSet<PetStatus> PetStatuses { get; set; }
+
+        public DbSet<PetFeature> PetFeatures { get; set; }
+
+        public DbSet<PetFeatureAssignment> PetFeatureAssignments { get; set; }
+
         public PetContext(DbContextOptions<PetContext> options)
             : base(options)
         {
@@ -17,9 +25,24 @@ namespace PetProject.DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Task>()
-                .HasOne<TaskType>(task => task.TaskType)
+                .HasOne(task => task.TaskType)
                 .WithMany(taskType => taskType.Tasks)
                 .HasForeignKey(task => task.TaskTypeId);
+
+            modelBuilder.Entity<Pet>()
+                .HasOne(pet => pet.PetStatus)
+                .WithMany(petStatus => petStatus.Pets)
+                .HasForeignKey(pet => pet.PetStatusId);
+
+            modelBuilder.Entity<PetFeatureAssignment>()
+                .HasOne(petFeatureAssignment => petFeatureAssignment.Pet)
+                .WithMany(pet => pet.PetFeatureAssignments)
+                .HasForeignKey(petFeatureAssignment => petFeatureAssignment.PetId);
+
+            modelBuilder.Entity<PetFeatureAssignment>()
+                .HasOne(petFeatureAssignment => petFeatureAssignment.PetFeature)
+                .WithMany(petFeature => petFeature.PetFeatureAssignments)
+                .HasForeignKey(petFeatureAssignment => petFeatureAssignment.PetFeatureId);
         }
     }
 }
