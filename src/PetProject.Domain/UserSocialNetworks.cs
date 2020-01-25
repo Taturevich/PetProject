c9657f1733d7fc215b57a66c8088128691fe06b8
Ivan <pollux094@gmail.com>
2020-01-25 12:09:41 +0300

    add user social link

---
+++ src/PetProject.Domain/UserSocialNetworks.cs
@@ -0,0 +1,11 @@
+﻿namespace PetProject.Domain
+{
+    public class UserSocialNetworks
+    {
+        public int UserId { get; set; }
+
+        public string NetworkType { get; set; }
+
+        public string Link { set; get; }
+    }
+}
