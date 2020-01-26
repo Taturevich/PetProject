8159f79a74585df547f3d52236ffc73c647c405b
D9bolic <46654130+D9bolic@users.noreply.github.com>
2020-01-26 09:32:15 +0300

    Added mock logic for ligon
    Tottaly ussles

--- src/PetProject.API/ClientApp/src/components/header/Header.tsx
+++ src/PetProject.API/ClientApp/src/components/header/Header.tsx
@@ -5,13 +5,30 @@ import Typography from '@material-ui/core/Typography';
 import petSearch from '../../static/images/petSearch.png';
 import petHelp from '../../static/images/petHelp.png';
 import petSaw from '../../static/images/petSaw.png';
+import { signInListInitialState } from '../../store/signIn/reducer';
 
-import { LoginModal, RegisterModal } from '../../components/modals';
-
-const images = [
+const notVolonteerImages = [
     {
         url: petSearch,
         title: 'Ищу питомца',
+        width: '30%',
+    },
+    {
+        url: petHelp,
+        title: 'Хочу помочь',
+        width: '30%',
+    },
+    {
+        url: petSaw,
+        title: 'Нашел бездомного питомца',
+        width: '40%',
+    },
+];
+
+const volonteerImages = [
+    {
+        url: petSearch,
+        title: '',
         width: '30%',
     },
     {
@@ -102,11 +119,76 @@ const useStyles = makeStyles(theme => ({
 
 export default function Header() {
     const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            {/* <LoginModal open={true} handleCancel={() => {}} handleLogin={() => {}} /> */}
-            {/* <RegisterModal open={true} handleCancel={() => {}} handleRegister={() => {}} /> */}
-        </div>
-    );
+    if (!!signInListInitialState.token) {
+        return (
+            <div className={classes.root}>
+                {notVolonteerImages.map(image => (
+                    <ButtonBase
+                        focusRipple
+                        key={image.title}
+                        className={classes.image}
+                        focusVisibleClassName={classes.focusVisible}
+                        style={{
+                            width: image.width,
+                        }}
+                    >
+                        <span
+                            className={classes.imageSrc}
+                            style={{
+                                backgroundImage: `url(${image.url})`,
+                            }}
+                        />
+                        <span className={classes.imageBackdrop} />
+                        <span className={classes.imageButton}>
+                            <Typography
+                                component="span"
+                                variant="subtitle1"
+                                color="inherit"
+                                className={classes.imageTitle}
+                            >
+                                {image.title}
+                                <span className={classes.imageMarked} />
+                            </Typography>
+                        </span>
+                    </ButtonBase>
+                ))}
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className={classes.root}>
+                {notVolonteerImages.map(image => (
+                    <ButtonBase
+                        focusRipple
+                        key={image.title}
+                        className={classes.image}
+                        focusVisibleClassName={classes.focusVisible}
+                        style={{
+                            width: image.width,
+                        }}
+                    >
+                        <span
+                            className={classes.imageSrc}
+                            style={{
+                                backgroundImage: `url(${image.url})`,
+                            }}
+                        />
+                        <span className={classes.imageBackdrop} />
+                        <span className={classes.imageButton}>
+                            <Typography
+                                component="span"
+                                variant="subtitle1"
+                                color="inherit"
+                                className={classes.imageTitle}
+                            >
+                                {image.title}
+                                <span className={classes.imageMarked} />
+                            </Typography>
+                        </span>
+                    </ButtonBase>
+                ))}
+            </div>
+        );
+    }
 }
