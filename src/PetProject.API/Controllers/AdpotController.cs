d3ffcf7199ba1beb21a3eaa89883b93e0eb43b32
D9bolic <46654130+D9bolic@users.noreply.github.com>
2020-01-26 13:58:14 +0300

    Added adopt controller stub

---
+++ src/PetProject.API/Controllers/AdpotController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdpotController : ControllerBase
+    {
+        [HttpPost]
+        [Route("{userId}/{petId}")]
+        public IActionResult AdoptPetForUser(int userId, int petId)
+        {
+            return Ok();
+        }
+    }
+}
